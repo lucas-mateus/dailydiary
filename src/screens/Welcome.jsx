4f16ab19dc3fb9dcf66ea867a1a2806f04442439
lucas-mateus <xlucasmat@gmail.com>
2022-12-03 17:18:30 -0300

    Atualizando tela de Welcome

--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,13 +1,14 @@
+import { Center, Text } from 'native-base'
 import React from 'react'
-import { View, Text } from 'react-native'
-import { Button } from '../components/Button'
+import {Card} from '../componetes/Card'
+import { Button } from '../componetes/Button'
 
 export function Welcome() {
   return (
-    	<View>
-            <Text> Seja muito bem-vindx ao seu diário!</Text>
-
-            <Button title={"Acessar Diário"} onPress={handleAuthentication}/>		
-	    </View>
+    	<Center flex={1} bgColor="light.50">
+            <Text textAlign={'center'} fontSize={'2xl'} color={'info.400'}fontWeight={'bold'}> Seja muito bem-vindx {'\n'} ao seu diário!</Text>
+            <Button  title={"Acessar Diário"} />		
+            <Text textAlign={'center'} px={8} mt={8}>Você terá acesso ao seu diário com {'\n'}a validação da sua digital!</Text>
+	    </Center>
      )
 }
