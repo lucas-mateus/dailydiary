af640a4f8a89113672396848ceee3fa89c0bc80e
lucas-mateus <xlucasmat@gmail.com>
2022-12-04 17:08:20 -0300

    Implementando tela de criação de diário

--- src/screens/CreateDiary.jsx
+++ src/screens/CreateDiary.jsx
@@ -2,21 +2,58 @@ import { View } from 'react-native'
 import React from 'react'
 import { Input } from '../components/Input'
 import { Button } from '../components/Button'
+import { useState } from 'react'
 
-export function CreateDiary() {
+export function CreateDiary({navigation}) {
+
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const [diaries, setNewDiaries] = useState([])
+
+
+  function clear(){
+    setContent('')
+    setTitle('')
+  }
+
+  const handleCreateDiary = () =>{
+    
+    if(title == '' || content == ''){
+      alert('Seu diário precisa ter um título e conteúdo!')
+      clear()
+      return
+    }
+
+    const date = new Date()
+	  const formatedDate = (`${date.getDay()}/${date.getMonth()}/${date.getYear()}`)
+	
+	  const data = {
+      id: String(date.getTime()),
+      date: formatedDate,
+      title:title,
+      content: content
+	}
+	setNewDiaries( oldState => [...oldState, data ])
+
+  navigation.navigate('Diários', {
+    screen: 'Home'
+  });
+}
   return (
     <View>
       <Input
         height={50}
         placeholder={'Seu título'}  
+        onChange={setTitle}
       />
       <Input
         multiline
         numberOfLines={4}
         height={300}
-        placeholder={'Digite seu diário aqui'}  
+        placeholder={'Digite seu diário aqui'}
+        onChange={setContent}
       />
-      <Button title={"Criar Diário"} />
+      <Button title={"Criar Diário"} onPress={handleCreateDiary} />
     </View>
   )
 }
