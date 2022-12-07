fd15a56db8dcf9bd2e2eaa3678dee5305da0608c
lucas-mateus <xlucasmat@gmail.com>
2022-12-07 14:13:55 -0300

    Colocando propriedade returnKeyType

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,35 +1,32 @@
-import { View, TextInput } from 'react-native'
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import { View, TextInput } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 
-export function Input({height,placeholder, ...rest }) {
-
+export function Input({ height, placeholder, ...rest }) {
   return (
-    <View style={[styles.inputWrapper, {height:height}]} >
+    <View style={[styles.inputWrapper, { height: height }]}>
       <TextInput
         {...rest}
         placeholder={placeholder}
-        placeholderTextColor={'#595959'}
+        placeholderTextColor={"#595959"}
+        returnKeyType="next"
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
+  inputWrapper: {
+    padding: 10,
+    backgroundColor: "#eaeaea",
+    width: 350,
+    marginTop: 40,
+    paddingHorizontal: 20,
+    marginHorizontal: 20,
+    borderBottomColor: "#0EA5E9",
+    borderBottomWidth: 2,
+    borderRadius: 4,
+  },
 
-    inputWrapper:{
-        padding: 10,
-        backgroundColor: '#eaeaea',
-        width: 350,
-        marginTop:40,
-        paddingHorizontal: 20,
-        marginHorizontal:20,
-        borderBottomColor: '#0EA5E9',
-        borderBottomWidth:2,
-        borderRadius:4
-    },
-
-    input:{
-        
-    }
-})
+  input: {},
+});
