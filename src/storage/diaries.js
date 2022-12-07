8fed45394af573df89941b4aa483e635c6f2a139
lucas-mateus <xlucasmat@gmail.com>
2022-12-06 23:06:08 -0300

    Melhorando sotrage e getData do Async Storage

--- src/storage/diaries.js
+++ src/storage/diaries.js
@@ -1,37 +1,35 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-const storeData = async (objectDiary) => {
+const storeData = async (newObjectDiaries) => {
+  
+  const oldDiaries = await getData();
 
   try {
-    const jsonValue = JSON.stringify(objectDiary)
-    await AsyncStorage.setItem('@diary_Key', jsonValue)
+    const jsonValue = JSON.stringify([newObjectDiaries, ...oldDiaries]);
+    await AsyncStorage.setItem("@diary", jsonValue);
   } catch (e) {
-      console.log('ERROR FROM STORAGE FUNCTION ====> ', e)
+    console.log(e);
   }
-
-}
+};
 
 const getData = async () => {
   try {
-    const jsonValue = await AsyncStorage.getItem('@diary_Key')
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch(e) {
-    console.log('ERROR FROM GET DATA FUNCTION ====> ', e)
+    const jsonValue = await AsyncStorage.getItem("@diary");
+    return jsonValue != null ? JSON.parse(jsonValue) : [];
+    
+  } catch (e) {
+    console.log(e);
   }
-
-}
+};
 
 const clearAll = async () => {
   try {
-    await AsyncStorage.clear()
-  } catch(e) {
+    await AsyncStorage.clear();
+  } catch (e) {
     // clear error
   }
 
-  console.log('Done.')
-}
-  
+  console.log("Done.");
+};
 
-
-export {storeData, getData, clearAll};
+export { storeData, getData, clearAll };
