cc3aaf1d501bbc238bada1ccbcb9ca6f51a12fd3
lucas-mateus <xlucasmat@gmail.com>
2022-12-07 14:15:19 -0300

    Corrigindo identação

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,41 +1,27 @@
-import React from 'react'
-import { 
-    StyleSheet, 
-    Text, 
-    TouchableOpacity,
-} from 'react-native'
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export function Button({title, ...rest }) {
-
-
-    return (
-        <TouchableOpacity
-            style={styles.button}
-            {...rest}
-            activeOpacity={0.6}
-        >
-            <Text style={styles.text}>{title}</Text>
-        </TouchableOpacity>
-    )
-
+export function Button({ title, ...rest }) {
+  return (
+    <TouchableOpacity style={styles.button} {...rest} activeOpacity={0.6}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 24,
-        color: '#fff',
-        fontWeight: 'bold'
-
-    },
-    button:{
-        padding: 10,
-        paddingHorizontal:20,
-        backgroundColor:'#0EA5E9',
-        borderRadius:10,
-        alignItems:'center',
-        marginTop:20,
-        marginHorizontal: 20
-    }
-
-
-})
+  text: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  button: {
+    padding: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#0EA5E9",
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 20,
+    marginHorizontal: 20,
+  },
+});
